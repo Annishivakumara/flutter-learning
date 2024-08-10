b3f765cd2668933d6f57546dcea4e6d94d7626e9
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-08-10 13:01:32 +0530

    Create 01-AppBar.dart

---
+++ 01-AppBar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+  
+      appBar: AppBar(
+      
+        title: Text('Well Come  to Kcet Councelling'),
+        backgroundColor: Colors.amberAccent,
+      ),
+    ),
+  ));
+}
