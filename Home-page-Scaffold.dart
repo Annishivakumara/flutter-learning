9c11d19d4af11dc8d0f0b0a47952cb8a5e127208
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-08-10 13:23:46 +0530

    Update Home-page-Scaffold.dart

--- Home-page-Scaffold.dart
+++ Home-page-Scaffold.dart
@@ -1,21 +1,29 @@
 Just app widget Learning
+ 1. from scartch try to solve 1 by one ? 
+   
   import 'package:flutter/material.dart';
-
-void main() => runApp(MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Kcet Councelling '),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
-      ),
+        void  main() => runApp(MaterialApp(
+                home: Scaffold(
+                appBar: AppBar(
+                title: Text('Kcet Councelling '),
+                centerTitle: true,
+                backgroundColor: Colors.red[600],
+            ),
+                  
       body: Center(
-        child: Text('Welcome to the councelling '),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Button clicked');
-        },
-        child: Text('click'),
-        backgroundColor: Colors.red[600],
+               child: Text('Welcome to the councelling '),
+            ),
+                  
+     floatingActionButton: FloatingActionButton(
+               onPressed: () {
+             print('Button clicked');
+          },
+            child: Text('click'),
+            backgroundColor: Colors.red[600],
       ),
     )));
+}
+
+
+
+
