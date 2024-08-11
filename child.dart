2e8f5a7fae641adfa007f24c9c7c8eaeac6b4f18
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-08-11 11:44:48 +0530

    Create child.dart

---
+++ child.dart
@@ -0,0 +1,16 @@
+Constructor of Container Class
+Syntax: 
+
+Container({Key key,
+           AlignmentGeometry alignment, 
+           EdgeInsetsGeometry padding, 
+           Color color, 
+           Decoration decoration, 
+           Decoration foregroundDecoration, 
+           double width, 
+           double height, 
+           BoxConstraints constraints, 
+           EdgeInsetsGeometry margin, 
+           Matrix4 transform, 
+           Widget child, 
+           Clip clipBehavior: Clip.none});
