120d163c9421851307a06849f8212af59d9b6ac7
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-08-13 07:37:58 +0530

    Create Drawer.dart

---
+++ Drawer.dart
@@ -0,0 +1,9 @@
+     #Drawer 
+     widget is used to provide access to different destinations and functionalities provided in your application. 
+     It is represented by three horizontal parallel lines on the upper end of the scaffold.  
+    It has a horizontal movement from the edge of the Scaffold that navigates the link to different routes in the flutter app.
+  
+ In order to use the app drawer you need to import the material component package that is ” package: flutter/material.dart.”
+ The Navigating AppDrawer is divided into three sections namely header, body, and footer.
+ The idea is about having a navigator with a couple of items as the drawer’s child that will be navigated to different destinations on getting tapped.
+All children of a Drawer widget are usually in ListView and initially, only the DrawerHeader is present in the UI which when tapped extends horizontally.
