de9f2ed233342e4f0ea8d4b1311ce87cca1189a5
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-08-27 18:43:36 +0530

    Update UI-widgets.dart

--- UI-widgets.dart
+++ UI-widgets.dart
@@ -10,3 +10,65 @@ In this article, exploring we will be exploring the same in detail.
 Design a TabController.
 Add tabs to the app.
 Add content in each tab.
+
+
+
+
+ Example:-
+
+  import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Smusic());
+}
+
+class Smusic extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 5,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.camera_alt)),
+                Tab(icon: Icon(Icons.music_video)),
+                Tab(icon: Icon(Icons.music_note)),
+                Tab(icon: Icon(Icons.grade)),
+                Tab(icon: Icon(Icons.email)),
+              ],
+            ),
+            title: Text('Smusic'),
+            centerTitle: true,
+            backgroundColor: Colors.blue,
+            toolbarHeight: 60.9,
+            toolbarOpacity: 0.9,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            elevation: 0.0,
+          ),
+          body: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16.0),
+              color: Color(0xffe7eef4),
+            ),
+            child: Text('Welcome to Smusic',
+                style: TextStyle(
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                )),
+          ),
+        ),
+  
+      ),
+    );
+  }
+}
