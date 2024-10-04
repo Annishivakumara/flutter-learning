5385045bf65663814b55a5fdef198acb53472965
Anni shivakumara <142305502+Annishivakumara@users.noreply.github.com>
2024-10-04 21:09:34 +0530

    Update and rename UI-widgets.dart to UI_widgets.dart

--- UI_widgets.dart
+++ UI_widgets.dart
@@ -1,18 +1,13 @@
 Flutter – Tabs
-
+--------------
 Tabs are exactly what you think it is. It’s part of the UI that navigates the user through different routes(ie, pages) when clicked upon.
 The use of tabs in applications is standard practice. Flutter provides a simple way to create tab layouts using the material library. 
 In this article, exploring we will be exploring the same in detail.
-
-
- To better understand the concept of tabs and their functionality in a Flutter app, let’s build a simple app with 5 tabs by following the below steps:
+To better understand the concept of tabs and their functionality in a Flutter app, let’s build a simple app with 5 tabs by following the below steps:
 
 Design a TabController.
 Add tabs to the app.
 Add content in each tab.
-
-
-
 
  Example:-
 
